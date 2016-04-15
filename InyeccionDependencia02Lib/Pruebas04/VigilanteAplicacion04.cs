42c62ee9499b079fd9b426a1358745b7376b3426
ElDillo <ElDillo@LosDillos.Com>
2016-04-15 14:58:25 -0400

    Se agregó VigilanteAplicacion04.

---
+++ InyeccionDependencia02Lib/Pruebas04/VigilanteAplicacion04.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Lib.Pruebas04 {
+   class VigilanteAplicacion04 {
+   } //class VigilanteAplicacion04
+} //namespace InyeccionDependencia02Lib.Pruebas04
