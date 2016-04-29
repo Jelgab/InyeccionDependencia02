8c0a8a018249192bbb75c2bced222ffd34d9c031
ElDillo <ElDillo@LosDillos.Com>
2016-04-29 17:14:17 -0400

    Se volvió pública la clase [DatosSimulados]

--- InyeccionDependencia02Lib/Pruebas05/DatosSimulados.cs
+++ InyeccionDependencia02Lib/Pruebas05/DatosSimulados.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace InyeccionDependencia02Lib.Pruebas05 {
-   class DatosSimulados : IDatosVarios {
+   public class DatosSimulados : IDatosVarios {
       public string ObtenerFrase( String lasOpciones ) {
          return "Solamante información simulada";
       }
