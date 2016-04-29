a5c0c13486487135f08ce78c20b7a70ae5d8c97c
ElDillo <ElDillo@LosDillos.Com>
2016-04-29 17:13:45 -0400

    Se agregó [ConsumidorDatos] al cual se le pueden inyectar instancias que cumplan [IDatosVarios]

---
+++ InyeccionDependencia02Lib/Pruebas05/ConsumidorDatos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Lib.Pruebas05 {
+   public class ConsumidorDatos {
+      private IDatosVarios _DatosServicioActual;
+
+      //Constructor:
+      public ConsumidorDatos( IDatosVarios instanciaParaDatos ) {
+         _DatosServicioActual = instanciaParaDatos;
+      }
+
+      //Métodos, etc:
+      public void MostrarDatos( String lasOpciones ) {
+         var resultadoObtenidoDeInstanciaParaDatosInyectada = _DatosServicioActual.ObtenerFrase( lasOpciones );
+
+         Console.WriteLine( $"Datos obtenidos: \r\n {resultadoObtenidoDeInstanciaParaDatosInyectada}" );
+      }
+
+   } //class ConsumidorDatos
+} //namespace InyeccionDependencia02Lib.Pruebas05
