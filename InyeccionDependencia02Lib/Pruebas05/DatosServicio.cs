3e10e1594640ec4e16b003fe09ebab7d40d1bf2e
ElDillo <ElDillo@LosDillos.Com>
2016-04-29 14:18:20 -0400

    Se agregaron: 1. IDatosVarios 2. DatosServicio  : IDatosVarios 3. DatosSimulados : IDatosVarios

---
+++ InyeccionDependencia02Lib/Pruebas05/DatosServicio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Lib.Pruebas05 {
+   public class DatosServicio : IDatosVarios {
+      public String ObtenerFrase( String lasOpciones ) {
+         var elResultado = "";
+
+         using ( var clienteRequest = new WebClient() ) {
+            var datosGet = "http://baconipsum.com/api/?type=meat-and-filler&paras=1"; //Servicio que devuelve palabras (como lorem ipsum)
+
+            try {
+               elResultado = clienteRequest.DownloadString( datosGet );
+            }
+            catch ( Exception errorHaciendoRequest ) {
+               elResultado = $"Hubo un error: {errorHaciendoRequest.Message}";
+            }
+         }
+
+         return elResultado;
+      } //public String ObtenerFrase( String lasOpciones )
+
+   } //class DatosServicio : IDatosVarios
+
+} //namespace InyeccionDependencia02Lib.Pruebas05
---
+++ InyeccionDependencia02Lib/Pruebas05/DatosSimulados.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Lib.Pruebas05 {
+   class DatosSimulados : IDatosVarios {
+      public string ObtenerFrase( String lasOpciones ) {
+         return "Solamante información simulada";
+      }
+   }
+}
