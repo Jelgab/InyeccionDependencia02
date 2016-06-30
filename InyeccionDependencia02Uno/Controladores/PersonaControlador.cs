b769b3c84fba8d4600fde196a2f4c441c83145e8
ElDillo <ElDillo@LosDillos.Com>
2016-06-30 09:46:04 -0400

    Se agregó Controladores/PersonaControlador.cs, con método [ListaNombresCompletos()]

---
+++ InyeccionDependencia02Uno/Controladores/PersonaControlador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Uno.Controladores {
+   public class PersonaControlador {
+      public static List<string> ListaNombresCompletos() {
+         //Cadena de conexión:
+         var conexionAdventureWorks = ConfigurationManager.ConnectionStrings[ "ConexionAdventureWorks" ].ConnectionString;
+
+         //Orden de SQL:
+         var scriptListaNombresCompletos = "SELECT TOP 50 FirstName + ' ' + ISNULL( MiddleName, ' ' ) + ' ' + LastName AS NombreCompleto FROM Person.Person";
+
+         //Adapter:
+         var PersonaAdaptador = new SqlDataAdapter( scriptListaNombresCompletos, conexionAdventureWorks );
+
+         //Obtener los datos:
+         var tablaNombresCompletos = new DataTable();
+         PersonaAdaptador.Fill( tablaNombresCompletos );
+
+         //Pasar a List<string>:
+         var resultadoListaNombresCompletos = tablaNombresCompletos.AsEnumerable().Select( datoActual => datoActual["NombreCompleto"].ToString() ).ToList();
+
+         return resultadoListaNombresCompletos;
+      }
+
+   } //public class PersonaControlador
+
+} //namespace InyeccionDependencia02Uno.Controladores
--- InyeccionDependencia02Uno/Inicial.cs
+++ InyeccionDependencia02Uno/Inicial.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InyeccionDependencia02Uno.Controladores;
 
 namespace InyeccionDependencia02Uno {
    class Inicial {
       static void Main( string[] args ) {
+         new Inicial().Prueba01();
       }
-   }
-}
+
+      private void Prueba01() {
+         var listaNombresCompletos = PersonaControlador.ListaNombresCompletos();
+      }
+   } //class Inicial
+
+} //namespace InyeccionDependencia02Uno
