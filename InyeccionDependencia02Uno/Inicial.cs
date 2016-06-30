9da5a2ea71e52e980a1daf67784d6482166cc4fa
ElDillo <ElDillo@LosDillos.Com>
2016-06-30 08:26:41 -0400

    Se agregó [InyeccionDependencia02Uno] para un ejemplo sencillo y compacto.

---
+++ InyeccionDependencia02Uno/Inicial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Uno {
+   class Inicial {
+      static void Main( string[] args ) {
+      }
+   }
+}
