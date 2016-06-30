5d6dca93c4986fd894ad95c3dd1d0cfb31c357a7
ElDillo <ElDillo@LosDillos.Com>
2016-06-30 15:09:07 -0400

    Se agregaron las clases IDatosPersona, DatosPersonaRegular, DatosPersonaMock01

---
+++ InyeccionDependencia02Uno/Lib/DatosPersonaMock01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDependencia02Uno.Lib {
+   class DatosPersonaMock01 : IDatosPersona {
+
+      public List<string> ObtenerListaNombresCompletos() {
+         return new List< string > { "Test name ABC", "Test name 123", "Test name I II III", "Test name A1", "Test name &", "Test name xyz", "Test name   ", };
+      }
+
+   } //class DatosPersonaMock01 : IDatosPersona
+} //namespace InyeccionDependencia02Uno.Lib
---
+++ InyeccionDependencia02Uno/Lib/DatosPersonaRegular.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InyeccionDependencia02Uno.Controladores;
+
+namespace InyeccionDependencia02Uno.Lib {
+   class DatosPersonaRegular : IDatosPersona {
+
+      public List< string > ObtenerListaNombresCompletos() {
+         var listaNombresCompletos = PersonaControlador.ListaNombresCompletos();
+         return listaNombresCompletos;
+      }
+
+   } //class DatosPersonaRegular : IDatosPersona
+} //namespace InyeccionDependencia02Uno.Lib {
